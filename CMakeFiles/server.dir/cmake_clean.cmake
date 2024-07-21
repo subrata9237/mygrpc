b76e62eb988770f3d6a677b958e9f7969c22192d
subrata9237 <bhuyan.subrata.bhuyan@gmail.com>
2024-07-21 13:13:16 +0530

    Initial commit

---
+++ CMakeFiles/server.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/server.dir/proto/helloworld.grpc.pb.cc.o"
+  "CMakeFiles/server.dir/proto/helloworld.pb.cc.o"
+  "CMakeFiles/server.dir/src/server.cpp.o"
+  "server"
+  "server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
